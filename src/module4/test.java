db5fd231832c7a907b72346eeffb824881917117
mbadalamente <70178700+mbadalamente@users.noreply.github.com>
2021-02-21 17:28:43 -0500

    changed questions

--- src/module4/test.java
+++ src/module4/test.java
@@ -7,6 +7,6 @@ public class test {
 		System.out.println("This is the foundation of my program!");
 		Scanner scan = new java.util.Scanner(System.in);
 		System.out.println("This program will ask you a series of questions about maddie");
-		String[] questions = {"What is maddie's favorite color?", "What is maddie's favorite number?", "What is maddie's favorite fruit?"};
+		String[] questions = {"What is maddie's least favorite color?", "What is maddie's favorite number?", "What is maddie's favorite fruit?", "What is maddie's middle name?"};
 	}
 }
